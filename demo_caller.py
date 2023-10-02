d657cf509a813a169d71090fc33d7018a06ce504
Eers <sreeaadhi07@gmail.com>
2023-10-03 00:40:30 +0530

    fixes

--- demo_caller.py
+++ demo_caller.py
@@ -1,27 +1,28 @@
-import tempfile
-import os
-import time
-import logging
-from vllm import LLM
-from pyngrok import ngrok
-from caller_agent.conversation import run_conversation
-from caller_agent.twilio_io import TwilioServer
-from caller_agent.agents import LlamaChatAgent, TwilioCaller
 from gevent import monkey
 
 monkey.patch_all()
 
+from caller_agent.agents import LlamaChatAgent, TwilioCaller
+from caller_agent.twilio_io import TwilioServer
+from caller_agent.conversation import run_conversation
+
+from pyngrok import ngrok
+from vllm import LLM
+
+import logging
+import time
+import os
+import tempfile
 
 logging.getLogger().setLevel(logging.INFO)
 
-port = 8080
+port=8080
 static_dir = os.path.join(tempfile.gettempdir(), "twilio_static")
 os.makedirs(static_dir, exist_ok=True)
 ngrok_http = ngrok.connect(port)
 remote_host = "826a-35-193-195-20.ngrok.io"
 
-logging.info(
-    f"Starting server at {remote_host} from local:{port}, serving static content from {static_dir}")
+logging.info(f"Starting server at {remote_host} from local:{port}, serving static content from {static_dir}")
 logging.info(f"Set call webhook to https://{remote_host}/incoming-voice")
 
 chat_llm = LLM(model="TheBloke/Llama-2-7B-chat-AWQ", quantization="awq")
@@ -33,15 +34,13 @@ agent_a = LlamaChatAgent(
     llm=chat_llm,
 )
 
-
 def run_chat(sess):
     agent_b = TwilioCaller(sess)
     while not agent_b.session.media_stream_connected():
         time.sleep(0.1)
     run_conversation(agent_a, agent_b)
 
-
 tws.on_session = run_chat
 
 # Outbound call
-# tws.start_call("+919952062221")
+#tws.start_call("+919952062221")
