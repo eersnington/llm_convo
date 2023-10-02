a7fd3d6ba09e852fafa7638f38586c52caec735c
Eers <sreeaadhi07@gmail.com>
2023-10-03 01:16:53 +0530

    test

--- demo_caller.py
+++ demo_caller.py
@@ -16,11 +16,11 @@ import tempfile
 
 logging.getLogger().setLevel(logging.INFO)
 
-port=8083
+port=8081
 static_dir = os.path.join(tempfile.gettempdir(), "twilio_static")
 os.makedirs(static_dir, exist_ok=True)
 ngrok_http = ngrok.connect(port)
-remote_host = "826a-35-193-195-20.ngrok.io"
+remote_host = ngrok_http.public_url.split("//")[1]
 
 logging.info(f"Starting server at {remote_host} from local:{port}, serving static content from {static_dir}")
 logging.info(f"Set call webhook to https://{remote_host}/incoming-voice")
